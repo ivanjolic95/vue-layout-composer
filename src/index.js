1139b7e398c38eca98da3471a09aebd81f5e2563
Ivan Jolic <ivan.jolic@metoda.com>
2019-11-14 11:40:41 +0100

    Fix icons loading

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import LayoutComposer from './components/LayoutComposer'
 import Layout from './components/LayoutComposer/components/Layout'
 import Cell from './components/LayoutComposer/components/Layout/components/Cell'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faArrowsAlt } from '@fortawesome/free-solid-svg-icons'
 
 import ComponentRegister from './plugins/ComponentRegister'
 
+library.add(faArrowsAlt)
+
 const components = {
+  FontAwesomeIcon,
   LayoutComposer,
   Layout,
   Cell
@@ -17,7 +22,7 @@ const plugin = (Vue, opts = { prefix: 'Lc' }) => {
   const compNames = Object.keys(components)
   for (let i = 0; i < compNames.length; i++) {
     const name = compNames[i]
-    if (name !== 'LayoutComposer')
+    if (name !== 'LayoutComposer' && name !== 'FontAwesomeIcon')
       Vue.component(`${prefix}${name}`, components[name])
     else
       Vue.component(name, components[name])
