958b732326585179190312a5a987d9f468cf1410
Ivan Jolic <ivan.jolic@metoda.com>
2019-11-29 16:04:45 +0100

    Fix eslint errors: Layout Utils

--- src/components/LayoutComposer/utils/layout.js
+++ src/components/LayoutComposer/utils/layout.js
@@ -1,11 +1,13 @@
+/* eslint-disable no-param-reassign */
 import _ from 'lodash'
 
 const addIds = (jsonConfig, startAt = 0) => {
   function _addIds(config) {
     if (!config) return
-  
-    config.id = startAt++
-  
+
+    config.id = startAt
+    startAt += 1
+
     if (!config.children) return
     config.children.forEach(el => {
       _addIds(el)
@@ -15,11 +17,11 @@ const addIds = (jsonConfig, startAt = 0) => {
   return _addIds(jsonConfig)
 }
 
-const removeIds = (jsonConfig) => {
+const removeIds = jsonConfig => {
   function _removeIds(config) {
     if (!config) return
 
-    delete config['id']
+    delete config.id
 
     if (!config.children) return
     config.children.forEach(el => {
@@ -43,30 +45,36 @@ const removeCell = (config, cellId) => {
 const addCell = (config, cell, parentId, prevSiblingId) => {
   if (config && config.id === parentId) {
     if (prevSiblingId) {
-      const prevSiblingIndex = config.children.findIndex(child => child && child.id === prevSiblingId)
+      const prevSiblingIndex = config.children.findIndex(
+        child => child && child.id === prevSiblingId
+      )
       config.children = [
         ...config.children.slice(0, prevSiblingIndex + 1),
         cell,
         ...config.children.slice(prevSiblingIndex + 1),
       ].filter(child => child)
     } else {
-      config.children = [
-        cell,
-        ...config.children,
-      ].filter(child => child)
+      config.children = [cell, ...config.children].filter(child => child)
     }
 
     return
   }
 
   if (config && config.children) {
-    for (let child of config.children) {
+    config.children.forEach(child => {
       addCell(child, cell, parentId, prevSiblingId)
-    }
+    })
   }
 }
 
-const moveElementToNewLayout = (cellConfig, prevParentLayoutJson, nextParentLayoutJson, cellId, parentId, prevSiblingId) => {
+const moveElementToNewLayout = (
+  cellConfig,
+  prevParentLayoutJson,
+  nextParentLayoutJson,
+  cellId,
+  parentId,
+  prevSiblingId
+) => {
   const newPrevParentLayoutJson = _.cloneDeep(prevParentLayoutJson)
   const newNextParentLayoutJson = _.cloneDeep(nextParentLayoutJson)
   removeCell(newPrevParentLayoutJson, cellId)
@@ -74,20 +82,54 @@ const moveElementToNewLayout = (cellConfig, prevParentLayoutJson, nextParentLayo
   return { newPrevParentLayoutJson, newNextParentLayoutJson }
 }
 
-const moveElementToNewPositionInLayout = (cellConfig, prevParentLayoutJson, cellId, parentId, prevSiblingId) => {
+const moveElementToNewPositionInLayout = (
+  cellConfig,
+  prevParentLayoutJson,
+  cellId,
+  parentId,
+  prevSiblingId
+) => {
   const newPrevParentLayoutJson = _.cloneDeep(prevParentLayoutJson)
   removeCell(newPrevParentLayoutJson, cellId)
   addCell(newPrevParentLayoutJson, cellConfig, parentId, prevSiblingId)
   return { newPrevParentLayoutJson, newNextParentLayoutJson: null }
 }
 
-const moveElementToNewPosition = (cellConfig, prevParentLayoutJson, nextParentLayoutJson, cellId, parentId, prevParentId, prevSiblingId) => {
-  let newPrevParentLayoutJson, newNextParentLayoutJson
-  
+const moveElementToNewPosition = (
+  cellConfig,
+  prevParentLayoutJson,
+  nextParentLayoutJson,
+  cellId,
+  parentId,
+  prevParentId,
+  prevSiblingId
+) => {
+  let newPrevParentLayoutJson
+  let newNextParentLayoutJson
+
   if (parentId !== prevParentId) {
-    ({ newPrevParentLayoutJson, newNextParentLayoutJson } = moveElementToNewLayout(cellConfig, prevParentLayoutJson, nextParentLayoutJson, cellId, parentId, prevSiblingId))
+    ;({
+      newPrevParentLayoutJson,
+      newNextParentLayoutJson,
+    } = moveElementToNewLayout(
+      cellConfig,
+      prevParentLayoutJson,
+      nextParentLayoutJson,
+      cellId,
+      parentId,
+      prevSiblingId
+    ))
   } else {
-    ({ newPrevParentLayoutJson, newNextParentLayoutJson } = moveElementToNewPositionInLayout(cellConfig, prevParentLayoutJson, cellId, parentId, prevSiblingId))
+    ;({
+      newPrevParentLayoutJson,
+      newNextParentLayoutJson,
+    } = moveElementToNewPositionInLayout(
+      cellConfig,
+      prevParentLayoutJson,
+      cellId,
+      parentId,
+      prevSiblingId
+    ))
   }
 
   return { newPrevParentLayoutJson, newNextParentLayoutJson }
